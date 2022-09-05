89c8d0af496956df1c7fd3e540cd39f4ab6c9ba5
MagomedFR <magomed.khaptukaev@gmail.com>
2022-09-05 12:52:09 +0300

    Node сделал

---
+++ first.js
@@ -0,0 +1,7 @@
+ function f(a, b) {
+    return a + b
+}
+let a = 1;
+let b = 28;
+
+export default {a,b, f}
---
+++ second.js
@@ -0,0 +1,3 @@
+import obj from './first.js';
+const {a,b, f} = obj
+console.log(a, b, f(15, 14))
